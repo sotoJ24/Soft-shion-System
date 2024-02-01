eb60a0eee4a79f1ef4cc1608a7e1e360f30d884c
sotoJ24 <94422368+sotoJ24@users.noreply.github.com>
2024-01-31 18:20:08 -0600

    Update editViewUserPassword.blade.php

--- resources/views/editViewUserPassword.blade.php
+++ resources/views/editViewUserPassword.blade.php
@@ -18,7 +18,7 @@
     <script src="{{asset('resourcesCrudUsers/js/script.js')}}"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-
+ 
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
