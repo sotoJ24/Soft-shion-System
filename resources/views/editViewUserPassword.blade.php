af16105942b13928f8574caebaf3863e8734ebdd
sotoJ24 <94422368+sotoJ24@users.noreply.github.com>
2024-01-31 18:20:01 -0600

    Update editViewUserPassword.blade.php

--- resources/views/editViewUserPassword.blade.php
+++ resources/views/editViewUserPassword.blade.php
@@ -3,7 +3,7 @@
 <head>   
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"> 
     <link rel="icon" href="{{asset('TriskeleIcon.png')}}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round"> 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
