d4714db90f45072d7032aba59543734e44ca1658
sotoJ24 <94422368+sotoJ24@users.noreply.github.com>
2024-02-02 15:00:29 -0600

    Update SupplierController.php

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Supplier;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\DB; 
 
 class SupplierController extends Controller
 {
