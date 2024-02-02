4b80dfa7375e7c0993786d5a027d3f00558da3e5
sotoJ24 <94422368+sotoJ24@users.noreply.github.com>
2024-02-02 15:00:19 -0600

    Update CategoryController.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Article;
-use Illuminate\Http\Request;
+use Illuminate\Http\Request; 
 use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
