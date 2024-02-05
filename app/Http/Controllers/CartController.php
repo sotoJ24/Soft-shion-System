444c180e57d7c1ab623f88c7a5b4f2967d0fbf68
sotoJ24 <94422368+sotoJ24@users.noreply.github.com>
2024-02-05 17:36:04 -0600

    Update CartController.php

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -39,7 +39,7 @@ class CartController extends Controller
             'cartItems'=>$cartItems
         ]);
 
-        return $pdf->download('cartInvoice.pdf');
+        return $pdf->download('cartInvoice.pdf'); 
     }
 
     /**
