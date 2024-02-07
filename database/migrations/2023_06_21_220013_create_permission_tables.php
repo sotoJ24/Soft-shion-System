0171496e04625d975daf736be69328c62fc196d2
sotoJ24 <94422368+sotoJ24@users.noreply.github.com>
2024-02-07 11:49:10 -0600

    Update 2023_06_21_220013_create_permission_tables.php

--- database/migrations/2023_06_21_220013_create_permission_tables.php
+++ database/migrations/2023_06_21_220013_create_permission_tables.php
@@ -16,7 +16,7 @@ class CreatePermissionTables extends Migration
     {
         $tableNames = config('permission.table_names');
         $columnNames = config('permission.column_names');
-        $teams = config('permission.teams');
+        $teams = config('permission.teams'); 
 
         if (empty($tableNames)) {
             throw new \Exception('Error: config/permission.php not loaded. Run [php artisan config:clear] and try again.');
