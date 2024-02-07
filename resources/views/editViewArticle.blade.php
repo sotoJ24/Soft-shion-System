4d4e9b691325ebad6ab708ef650d17934b2a99dc
sotoJ24 <94422368+sotoJ24@users.noreply.github.com>
2024-02-07 11:50:50 -0600

    Update editViewArticle.blade.php

--- resources/views/editViewArticle.blade.php
+++ resources/views/editViewArticle.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8"> 
     <link rel="icon" href="{{asset('TriskeleIcon.png')}}">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
