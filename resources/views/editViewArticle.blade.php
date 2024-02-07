a2b2b5fc891951dcc8f2dd5fa930ac116ef52184
sotoJ24 <94422368+sotoJ24@users.noreply.github.com>
2024-02-07 11:50:35 -0600

    Update editViewArticle.blade.php

--- resources/views/editViewArticle.blade.php
+++ resources/views/editViewArticle.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
     <link rel="stylesheet" href="{{asset('resourcesEditUser/css/styles.css')}}">  
 
