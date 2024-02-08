c47c0ac7195102f0e635109f64e3cb74a0072710
sotoJ24 <94422368+sotoJ24@users.noreply.github.com>
2024-02-08 11:12:08 -0600

    Update 2023_06_29_203653_create_suppliers_table.php

--- database/migrations/2023_06_29_203653_create_suppliers_table.php
+++ database/migrations/2023_06_29_203653_create_suppliers_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('company_name',100);
             $table->string('phone_number',30);
-            $table->string('email')->unique();
+            $table->string('email')->unique();  
             $table->string('contact_name',100);
             $table->string('address',150);
             $table->timestamps();
