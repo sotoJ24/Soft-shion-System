6313c5f8075065b0981dcf3f3d62c16390e18391
sotoJ24 <94422368+sotoJ24@users.noreply.github.com>
2024-02-10 20:45:44 -0600

    Update script.js

--- public/resourcesCrudUsers/js/script.js
+++ public/resourcesCrudUsers/js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 	$("#selectAll").click(function(){
 		if(this.checked){
 			checkbox.each(function(){ 
-				this.checked = true;
+				this.checked = true; 
 			});
 		} else{
 			checkbox.each(function(){
