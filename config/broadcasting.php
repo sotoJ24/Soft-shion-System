164d981cf26df8c3a7ea46ef850cef31d514d46f
sotoJ24 <94422368+sotoJ24@users.noreply.github.com>
2024-02-10 20:46:02 -0600

    Update broadcasting.php

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -25,7 +25,7 @@ return [
     | Here you may define all of the broadcast connections that will be used
     | to broadcast events to other systems or over websockets. Samples of
     | each available type of connection are provided inside this array.
-    |
+    | 
     */
 
     'connections' => [
