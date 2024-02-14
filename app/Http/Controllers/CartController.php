64d1d7ad05a1aa8048cb14e53b640ec5467424f4
sotoJ24 <94422368+sotoJ24@users.noreply.github.com>
2024-02-13 19:19:00 -0600

    Update CartController.php

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -48,7 +48,7 @@ class CartController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+     public function store(Request $request)
     {
 
         $productId = $request->input('product_id');
@@ -56,21 +56,21 @@ class CartController extends Controller
         $addOrSubtract = $request->input('addOrSubtract');
         $product = Article::findOrFail($productId);
 
-        $cart = session()->get('cart', []); 
+
+        $cart = session()->get('cart', []);
 
         if (isset($cart[$productId]) && ($quantity < $product->quantity)) {
 
-            if(!(($cart[$productId]['quantity'] <= 1 && $addOrSubtract == 0) || ($cart[$productId]['quantity'] >= $product->quantity && $addOrSubtract==1))){
-
+            if($this->isValidQuantity($cart, $productId, $addOrSubtract, $quantity, $product->quantity)){
                 $quantityResult =  $addOrSubtract
-                                ? $cart[$productId]['quantity'] += $quantity
-                                : $cart[$productId]['quantity'] -= $quantity;
+                        ?   $cart[$productId]['quantity'] += $quantity
+                        :   $cart[$productId]['quantity'] -= $quantity;
 
                 $cart[$productId]['product_price'] = $product->article_price * $quantityResult;
             }
         }
-        else if($quantity < $product->quantity){ 
-            $cart[$productId] = [ 
+        else if($quantity < $product->quantity){
+            $cart[$productId] = [
                 'product' => $product,
                 'quantity' => $quantity,
                 'product_id' => $product->id,
@@ -84,13 +84,25 @@ class CartController extends Controller
             return "Quantity not in stock";
         }
 
-        // $product->quantity -= $quantity;
-        // $product->save();
-
         session()->put('cart', $cart);
 
         return redirect()->route('get_cart_route');
     }
+
+
+    public function isValidQuantity($cart, $productId, $addOrSubtract, $quantity, $productQuantity) {
+        $currentQuantity = $cart[$productId]['quantity'];
+
+        if (!(($currentQuantity <= 1 && $addOrSubtract == 0) ||
+            ($currentQuantity >= $productQuantity && $addOrSubtract == 1)))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    
 
     /**
      * Display the specified resource.
