fa1228c524e16520538895fc4f94eb5d5d206191
sotoJ24 <94422368+sotoJ24@users.noreply.github.com>
2024-02-15 13:25:35 -0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -36,9 +36,6 @@ Route::post('create/category',[CategoryController::class, 'store'])->name('store
 Route::put('update/category/{id}',[CategoryController::class, 'update'])->name('update_category_route');
 Route::delete('delete/category/{id}',[CategoryController::class, 'destroy'])->name('delete_category_route');
 
-/*--------------------------------------------Cart---------------------------------------*/
-
-
 /*--------------------------------------------Customers---------------------------------------*/
 Route::get('customer',[CustomerController::class, 'index'])->name('get_customer_route');
 Route::post('create/customer',[CustomerController::class, 'store'])->name('store_customer_route');
