07553033f3d740c1defff9d3cd47247c6933208b
Josué Soto Herrera <94422368+sotoJ24@users.noreply.github.com>
2024-02-21 19:46:18 -0600

    Update 2024_02_21_021815_create_inventories_table.php

--- database/migrations/2024_02_21_021815_create_inventories_table.php
+++ database/migrations/2024_02_21_021815_create_inventories_table.php
@@ -1,1 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->id();
+            $table->string('article_code')->unique();
+            $table->string('article_name')->max(50);
+            $table->integer('quantity');
+            $table->date('reception_date');
+            $table->integer('price_by_unit');
+            $table->string('unit_of_measurement')->max(20);
+            $table->unsignedBigInteger('supplier_id');
+            $table->foreign('supplier_id')->references('id')->on('suppliers')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventories');
+    }
+};
 
