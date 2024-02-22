b8ebabac21453b39fcdc64224f6747481a7ccccc
Josué Soto Herrera <94422368+sotoJ24@users.noreply.github.com>
2024-02-21 19:49:54 -0600

    Update 2024_02_21_230511_create_deliveries_table.php

--- database/migrations/2024_02_21_230511_create_deliveries_table.php
+++ database/migrations/2024_02_21_230511_create_deliveries_table.php
@@ -1,1 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deliveries', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_customer_id');
+            $table->string('address');
+            $table->dateTime('delivery_date');
+            $table->foreign('user_customer_id')->references('id')->on('user_customers')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deliveries');
+    }
+};
 
