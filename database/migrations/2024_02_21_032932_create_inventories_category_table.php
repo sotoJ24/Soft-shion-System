b55c9369e62664df638f2cfa0761b74a499511fc
Josué Soto Herrera <94422368+sotoJ24@users.noreply.github.com>
2024-02-21 19:47:42 -0600

    Update 2024_02_21_032932_create_inventories_category_table.php

--- database/migrations/2024_02_21_032932_create_inventories_category_table.php
+++ database/migrations/2024_02_21_032932_create_inventories_category_table.php
@@ -1,1 +1,35 @@
+<?php
 
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventories_category', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('inventory_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('inventory_id')->references('id')->on('inventories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventories_category');
+    }
+};
