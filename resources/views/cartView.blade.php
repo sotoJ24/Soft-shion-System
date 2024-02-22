71240231e913ee1406cd7cb27572d2f04f923a88
Josué Soto Herrera <94422368+sotoJ24@users.noreply.github.com>
2024-02-22 17:23:26 -0600

    Update cartView.blade.php

--- resources/views/cartView.blade.php
+++ resources/views/cartView.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge"> 
-    <link rel="icon" href="{{asset('TriskeleIcon.png')}}"> 
+    <link rel="icon" href="{{asset('TriskeleIcon.png')}}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"> --}}
