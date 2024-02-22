5c3b20baa4a747d7b359c2d788875408285237ca
Josué Soto Herrera <94422368+sotoJ24@users.noreply.github.com>
2024-02-21 19:48:48 -0600

    Update 2024_02_21_230510_create_user_customers_table.php

--- database/migrations/2024_02_21_230510_create_user_customers_table.php
+++ database/migrations/2024_02_21_230510_create_user_customers_table.php
@@ -1,1 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('address');
+            $table->string('phone_number')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_customers');
+    }
+};
 
