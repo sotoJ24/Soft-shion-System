09abd4da40359cb54d31f8f81b7ef564568716d0
Josué Soto Herrera <94422368+sotoJ24@users.noreply.github.com>
2024-03-03 18:49:09 -0600

    Update CatalogController.php

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -8,7 +8,7 @@ use App\Models\Supplier;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
-
+ 
 class CatalogController extends Controller
 {
     public function __construct()
