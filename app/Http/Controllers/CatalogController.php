c89f9fd8ede3d49a1b8a06b4330059ed19803b8c
Josué Soto Herrera <94422368+sotoJ24@users.noreply.github.com>
2024-03-05 12:44:15 -0600

    Update CatalogController.php

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
  
 use App\Models\Catalog; 
 use App\Models\Category;
-use App\Models\Supplier;
+use App\Models\Supplier; 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
