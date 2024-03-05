d7d4136c30f974945885a7ef3e47b9eda2871477
Josué Soto Herrera <94422368+sotoJ24@users.noreply.github.com>
2024-03-05 12:43:23 -0600

    Update CatalogController.php

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -13,7 +13,7 @@ class CatalogController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth'); 
     }
 
     private function manageViewArticles($articles,$categories,$suppliers)
