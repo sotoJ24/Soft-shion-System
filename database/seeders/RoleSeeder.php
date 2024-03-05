4dfa9bdfd5f459ec80b8dc856f52eb277a7d7ecf
Josué Soto Herrera <94422368+sotoJ24@users.noreply.github.com>
2024-03-05 12:52:39 -0600

    Update RoleSeeder.php

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder; 
 use Spatie\Permission\Models\Role;
-use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Permission; 
 
 class RoleSeeder extends Seeder 
 {
