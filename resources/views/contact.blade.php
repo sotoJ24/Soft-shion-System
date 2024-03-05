0785be221b43a165d40939e43c3efc9b1e171b6b
Josué Soto Herrera <94422368+sotoJ24@users.noreply.github.com>
2024-03-04 18:48:18 -0600

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -35,7 +35,7 @@
 
 	<!-- Icomoon Icon Fonts-->
 	{{-- <link rel="stylesheet" href="css/icomoon.css"> --}} 
-    <link href="{{asset('resourcesClothingStore/css/icomoon.css')}}" rel="stylesheet" />
+    <link href="{{asset('resourcesClothingStore/css/icomoon.css')}}" rel="stylesheet" /> 
 
 
 	<!-- Themify Icons-->
