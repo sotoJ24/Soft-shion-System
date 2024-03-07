3794a88cc0ee50e3f714264d4038bbb7ec08b274
Josué Soto Herrera <94422368+sotoJ24@users.noreply.github.com>
2024-03-07 09:04:34 -0600

    Update CatalogController.php

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -1,62 +1,62 @@
- <?php
- 
-namespace App\Http\Controllers; 
- 
-use App\Models\Catalog; 
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Catalog;
 use App\Models\Category;
-use App\Models\Supplier;  
+use App\Models\Supplier;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;  
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
- 
-class CatalogController extends Controller 
+
+class CatalogController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth'); 
+        $this->middleware('auth');
     }
 
     private function manageViewArticles($articles,$categories,$suppliers)
-    {  
+    {
 
         return view('crudArticles', compact('articles','categories','suppliers'));
     }
- 
+
     private function getArticlesWithSupplier()
     {
-        return Catalog::with('supplier')->get(); 
+        return Catalog::with('supplier')->get();
     }
 
-    private function getSuppliers() 
+    private function getSuppliers()
     {
-        return Supplier::all(); 
-    } 
+        return Supplier::all();
+    }
 
-    private function getCategories() 
+    private function getCategories()
     {
         return Category::all();
     }
- 
-    /** 
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
-    { 
+    {
         return $this->manageViewArticles(
                                          $this->getArticlesWithSupplier(),
                                          $this->getCategories(),
                                          $this->getSuppliers()
-                                        ); 
-    } 
+                                        );
+    }
 
 
     public function filter(Request $request)
     {
         $search = $request->search;
-        $articles = Catalog::where('article_code','LIKE','%'.$search.'%')
-                    ->orWhere('article_name','LIKE','%'.$search.'%')
+        $articles = Catalog::where('product_code','LIKE','%'.$search.'%')
+                    ->orWhere('product_name','LIKE','%'.$search.'%')
                     ->orWhere('size','LIKE','%'.$search.'%')
                     ->get();
 
@@ -97,9 +97,9 @@ class CatalogController extends Controller
 
 
         $validateData =  Validator::make($request->all(),[
-            'article_code' => 'required|string|unique:articles',
-            'article_name' => 'required|string|max:27',
-            'article_price' => 'required|numeric|regex:/^\d+(\.\d{1,2})?$/',
+            'product_code' => 'required|string|unique:catalogs',
+            'product_name' => 'required|string|max:27',
+            'product_price' => 'required|numeric|regex:/^\d+(\.\d{1,2})?$/',
             'quantity' => 'required|integer',
             'size' => 'required|string',
             'description' => 'required|string',
@@ -112,16 +112,16 @@ class CatalogController extends Controller
 
         if(!$validateData->fails()){
             $article = Catalog::create([
-                'article_code' => $validateData->validated()['article_code'],
-                'article_name' => $validateData->validated()['article_name'],
-                'article_price' => $validateData->validated()['article_price'],
+                'product_code' => $validateData->validated()['product_code'],
+                'product_name' => $validateData->validated()['product_name'],
+                'product_price' => $validateData->validated()['product_price'],
                 'quantity' =>  $validateData->validated()['quantity'],
                 'size' => $validateData->validated()['size'],
                 'description' => $validateData->validated()['description'],
                 'image' =>  $fileNameImage,
                 'supplier_id' => $supplier_id
             ]);
-            $article->articlesCategory()->attach($request['categories']);
+            $article->productsCategory()->attach($request['categories']);
             return redirect()->back();
         }
 
@@ -149,12 +149,12 @@ class CatalogController extends Controller
      * @param  \App\Models\Catalog  $catalog
      * @return \Illuminate\Http\Response
      */
-    public function edit(Article $article)
+    public function edit(Catalog $catalog)
     {
 
         $categories = $this->getCategories();
         $suppliers = $this->getSuppliers();
-        return view('editViewArticle', compact('article','categories','suppliers'));
+        return view('editViewArticle', compact('catalog','categories','suppliers'));
     }
 
     /**
@@ -176,9 +176,9 @@ class CatalogController extends Controller
 
 
         $validateData =  Validator::make($request->all(),[
-            'article_code' => 'required|string|unique:articles,article_code,'.$id,
-            'article_name' => 'required|string|max:27',
-            'article_price' => 'required|numeric|regex:/^\d+(\.\d{1,2})?$/',
+            'product_code' => 'required|string|unique:catalogs,product_code,'.$id,
+            'product_name' => 'required|string|max:27',
+            'product_price' => 'required|numeric|regex:/^\d+(\.\d{1,2})?$/',
             'quantity' => 'required|integer',
             'size' => 'required|string',
             'description' => 'required|string',
@@ -195,9 +195,9 @@ class CatalogController extends Controller
         }
 
         $article->update([
-            'article_code' => $validateData->validated()['article_code'],
-            'article_name' => $validateData->validated()['article_name'],
-            'article_price' => $validateData->validated()['article_price'],
+            'product_code' => $validateData->validated()['product_code'],
+            'product_name' => $validateData->validated()['product_name'],
+            'product_price' => $validateData->validated()['product_price'],
             'quantity' => $validateData->validated()['quantity'],
             'size' => $validateData->validated()['size'],
             'description' => $validateData->validated()['description'],
@@ -205,7 +205,7 @@ class CatalogController extends Controller
             'supplier_id' => $supplier_id
         ]);
 
-        $article->articlesCategory()->sync($request['categories']);
+        $article->productsCategory()->sync($request['categories']);
         return redirect('manage/articles')->with('success','Successfully Updated');
     }
 
@@ -223,6 +223,6 @@ class CatalogController extends Controller
             $article->delete();
             return redirect()->back();
         }
-        return response()->json(['Error' => 'Article not found'], 404);
+        return response()->json(['Error' => 'Product not found'], 404);
     }
 }
