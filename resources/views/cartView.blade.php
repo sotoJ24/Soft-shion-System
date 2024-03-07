28602f6c131fd5a53208fbe7c53ea1de0ae1a804
Josué Soto Herrera <94422368+sotoJ24@users.noreply.github.com>
2024-03-06 22:30:40 -0600

    Update cartView.blade.php

--- resources/views/cartView.blade.php
+++ resources/views/cartView.blade.php
@@ -7,7 +7,7 @@
     <link rel="icon" href="{{asset('TriskeleIcon.png')}}"> 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="{{asset('resourcesCartView/css/styles.css')}}">
+    <link rel="stylesheet" href="{{asset('resourcesCartView/css/styles.css')}}"> 
     <title>Cart Shopping</title>
 </head>  
 <body>
