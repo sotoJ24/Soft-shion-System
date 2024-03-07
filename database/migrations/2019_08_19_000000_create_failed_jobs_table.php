f90961ec7d52c81f2fd7f8502a25b452d0cb49a9
Josué Soto Herrera <94422368+sotoJ24@users.noreply.github.com>
2024-03-06 22:28:01 -0600

    Update 2019_08_19_000000_create_failed_jobs_table.php

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
      * @return void
      */
     public function up()
-    {
+    { 
         Schema::create('failed_jobs', function (Blueprint $table) {
             $table->id();
             $table->string('uuid')->unique();
