c9858ce930c253fd6ba5d9b71a1bee61aa238a05
Josué Soto Herrera <94422368+sotoJ24@users.noreply.github.com>
2024-03-09 14:42:35 -0600

    Update AuthenticationController.php

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Validator;
-use App\Models\User;
+use App\Models\User;  
 
 
 class AuthenticationController extends Controller 
