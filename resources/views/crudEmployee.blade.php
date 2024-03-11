8d362f2faa4a915ea968ba5b23e3a06a8bc73442
Josué Soto Herrera <94422368+sotoJ24@users.noreply.github.com>
2024-03-11 11:48:05 -0600

    Update crudEmployee.blade.php

--- resources/views/crudEmployee.blade.php
+++ resources/views/crudEmployee.blade.php
@@ -7,7 +7,7 @@
     <title>Manage Employee</title> 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"> 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="{{asset('resourcesSections/css/styles.css')}}">
 
