e6f7d7468fc1638197f8e2f8fd5395129d848867
Josué Soto Herrera <94422368+sotoJ24@users.noreply.github.com>
2024-03-14 16:29:14 -0600

    Update AuthenticationController.php

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
  
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;  
+use Illuminate\Support\Facades\Auth;   
 use Illuminate\Support\Facades\Hash; 
 use Illuminate\Support\Facades\DB; 
 use Illuminate\Support\Facades\Crypt;  
