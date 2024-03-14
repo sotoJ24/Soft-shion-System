de880c71af25c2b4e08abdd05800ff98fbff7c67
Josué Soto Herrera <94422368+sotoJ24@users.noreply.github.com>
2024-03-14 16:28:08 -0600

    Update AuthenticationController.php

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -14,7 +14,7 @@ use App\Models\User;
 class AuthenticationController extends Controller 
 {
 
-    public function index()
+    public function index() 
     { 
         return view('loginView'); 
     }
