ff98aa80d2b8c86eb72b8f900298fe6deb11d1e9
Josué Soto Herrera <94422368+sotoJ24@users.noreply.github.com>
2024-03-14 16:29:48 -0600

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -21,7 +21,7 @@
 	<meta property="og:site_name" content=""/>
 	<meta property="og:description" content=""/> 
 	<meta name="twitter:title" content="" />
-	<meta name="twitter:image" content="" />
+	<meta name="twitter:image" content="" /> 
 	<meta name="twitter:url" content="" /> 
 	<meta name="twitter:card" content="" />
 
