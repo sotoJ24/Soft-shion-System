d631852202bdf5fdf74f0a9b8fd1c74d82260973
Josué Soto Herrera <94422368+sotoJ24@users.noreply.github.com>
2024-04-02 00:36:35 -0600

    Update 2023_06_29_202150_create_categories_table.php

--- database/migrations/2023_06_29_202150_create_categories_table.php
+++ database/migrations/2023_06_29_202150_create_categories_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) { 
             $table->id();
             $table->string('category_name')->max(50)->nullable(false);
             $table->string('description');
