aac5dea5957df67e64ffe1163e85cce0999859a1
Josué Soto Herrera <94422368+sotoJ24@users.noreply.github.com>
2024-04-02 00:35:02 -0600

    Update 2023_07_01_193014_create_product_category_table.php

--- database/migrations/2023_07_01_193014_create_product_category_table.php
+++ database/migrations/2023_07_01_193014_create_product_category_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('product_category', function (Blueprint $table) {
             $table->id(); 
-            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('category_id'); 
             $table->unsignedBigInteger('product_code');
             $table->foreign('product_code')->references('id')->on('catalog')->onDelete('cascade');
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
